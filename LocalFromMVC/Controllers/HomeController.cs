18a88711cd8c8ace3dcec706c9df192e0e92f45c
Saifur Rahman - LP <saifur.rahman@enosisbd.com>
2023-01-24 14:53:04 +0600

    added culture in nav bar with partial using localizer and cookie

--- LocalFromMVC/Controllers/HomeController.cs
+++ LocalFromMVC/Controllers/HomeController.cs
@@ -1,22 +1,48 @@
 ﻿using LocalFromMVC.Models;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
 using System.Diagnostics;
+using System.Net;
 
 namespace LocalFromMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHtmlLocalizer<HomeController> _localizer;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHtmlLocalizer<HomeController> localizer)
         {
             _logger = logger;
+            _localizer = localizer;
         }
 
         public IActionResult Index()
         {
+            var test = _localizer["HelloWorld"];
+            ViewData["HelloWorld"] = test;
             return View();
         }
+
+        [HttpPost]
+        public IActionResult CultureManagement(string culture, string returnUrl)
+        {
+            Response.Cookies
+                .Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(
+                    new RequestCulture(culture)
+                    ),
+                new CookieOptions
+                {
+                    Expires = DateTimeOffset.Now.AddDays(30)
+                }
+                );
+            //return RedirectToAction(nameof(Index));
+            return LocalRedirect(returnUrl);
+        }
+
 
         public IActionResult Privacy()
         {
--- LocalFromMVC/Program.cs
+++ LocalFromMVC/Program.cs
@@ -1,7 +1,34 @@
+using Microsoft.Extensions.Options;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Resources";
+});
+builder.Services
+    .AddMvc()
+    .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new List<CultureInfo>
+    {
+        new CultureInfo("en"),
+        new CultureInfo("fr"),
+        new CultureInfo("es")
+    };
+    options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+
+});
+
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
@@ -19,6 +46,18 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+
+app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);   
+/* 
+ * Covered in builder.Services middleware and the above line
+ */
+//var supportedCultures = new[] { "en", "fr", "es" };
+//var localOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+//    .AddSupportedCultures(supportedCultures)
+//    .AddSupportedUICultures(supportedCultures);
+//app.UseRequestLocalization(localOptions);
+
 
 app.MapControllerRoute(
     name: "default",
