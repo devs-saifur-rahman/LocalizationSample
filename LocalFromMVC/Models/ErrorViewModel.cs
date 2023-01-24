df173cd772a45808f1d688ec130c13805f9c51ba
Saifur Rahman - LP <saifur.rahman@enosisbd.com>
2023-01-24 12:48:45 +0600

    New Project added to test another approach

---
+++ LocalFromMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LocalFromMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
