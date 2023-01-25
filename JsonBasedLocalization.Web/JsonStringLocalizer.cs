bff638d8177320c6fa0aa576b5717fa03dac4b96
Saifur Rahman - LP <saifur.rahman@enosisbd.com>
2023-01-25 11:19:21 +0600

    Json based localization with localization factory

---
+++ JsonBasedLocalization.Web/JsonStringLocalizer.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
+
+namespace JsonBasedLocalization.Web
+{
+    public class JsonStringLocalizer : IStringLocalizer
+    {
+        private readonly JsonSerializer _serializer = new();
+        public LocalizedString this[string name]
+        {
+            get
+            {
+                var value = GetString(name);
+                return new LocalizedString(name, value);
+            }
+        }
+
+        public LocalizedString this[string name, params object[] arguments]
+        {
+            get
+            {
+                var actualValue = this[name];
+                return !actualValue.ResourceNotFound
+                    ? new LocalizedString(name, string.Format(actualValue.Value, arguments))
+                    : actualValue;
+            }
+        }
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            var filePath = $"Resources/{Thread.CurrentThread.CurrentCulture.Name}.json";
+
+            using FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            using StreamReader streamReader = new StreamReader(stream);
+            using JsonTextReader reader = new JsonTextReader(streamReader);
+
+            while (reader.Read())
+            {
+                if (reader.TokenType != JsonToken.PropertyName)
+                {
+                    continue;
+                }
+                var key = reader.Value as string;
+                reader.Read();
+                var value = _serializer.Deserialize<string>(reader);
+                yield return new LocalizedString(key, value);
+            }
+        }
+
+        private string GetString(string key)
+        {
+            //Resources/ar-EG.json
+            //Resources/en-US.json
+            var filePath = $"Resources/{Thread.CurrentThread.CurrentCulture.Name}.json";
+            var fullFilePath = Path.GetFullPath(filePath);
+            if (File.Exists(fullFilePath))
+            {
+                var result = GetValueFromJSON(key, fullFilePath);
+                return result;
+            }
+            return String.Empty;
+
+        }
+
+        public string GetValueFromJSON(string propertyName, string filePath)
+        {
+            if (String.IsNullOrEmpty(propertyName) || String.IsNullOrEmpty(filePath))
+            {
+                return String.Empty;
+            }
+
+            using FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            using StreamReader streamReader = new StreamReader(stream);
+            using JsonTextReader reader = new JsonTextReader(streamReader);
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.PropertyName && reader.Value as string == propertyName)
+                {
+                    reader.Read();
+                    return _serializer.Deserialize<string>(reader);
+                }
+            }
+
+            return String.Empty;
+
+        }
+    }
+}
